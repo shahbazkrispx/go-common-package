0f07ceb4e4d40eb2ab4a20d9cd73eea72192d3f6
shahbazkrispx <shahbaz.khan@krispx.com>
2021-01-20 14:51:42 +0500

    initial commit

--- respond.go
+++ respond.go
@@ -3,13 +3,15 @@ package respond
 type Response struct {
 	Status bool `json:"status"`
 	Message string `json:"message"`
-	Data interface{} `json:"data"`
+	Data interface{} `json:"data,omitempty"`
+	Error interface{} `json:"error,omitempty"`
 }
 
-func Builder(status bool, msg string, data interface{}) Response {
+func Builder(status bool, msg string, data interface{}, errors interface{}) Response {
 	return Response{
 		Status: status,
 		Message: msg,
 		Data: data,
+		Error: errors,
 	}
 }
